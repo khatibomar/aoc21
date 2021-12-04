628222dd7a4d491ebf2f7462be4d3f480f361954
OmarElKhatibCS <omarelkhatib@mail.com>
2021-12-04 23:56:53 +0200

    Solve day 02 part 1

---
+++ day02/solve.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	FORWARD = "forward"
+	UP      = "up"
+	DOWN    = "down"
+)
+
+var (
+	UNKNOW_CMD = errors.New("Unkown Command")
+)
+
+func solve(filename string) (int, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return -1, err
+	}
+	var cmd string
+	var val, depth, hori int
+	for {
+		_, err := fmt.Fscanf(f, "%s%d", &cmd, &val)
+		if err != nil {
+			break
+		}
+		switch cmd {
+		case FORWARD:
+			hori += val
+		case UP:
+			depth -= val
+		case DOWN:
+			depth += val
+		default:
+			return -1, UNKNOW_CMD
+		}
+	}
+	return depth * hori, nil
+}
+
+func solve2(filename) (int, error) {}
+
+func main() {
+	ans, err := solve("input.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(ans)
+}
